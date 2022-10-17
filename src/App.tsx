de293d3afa0d5c5c954eb264460567dc5062b7cd
Lugp <lugpdev2022@gmail.com>
2022-10-16 19:36:43 -0500

    Add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-      <h1>Calculator App</h1>
-    </>
+    <div className='container'>
+      <div className='row'>
+        <h1 className='col-12 text-center mt-4'>Calculator App</h1>
+      </div>
+    </div>
   );
 }
 
