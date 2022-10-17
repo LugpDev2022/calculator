e1e02a649a3932259e24089014825a61033b2e80
Lugp <lugpdev2022@gmail.com>
2022-10-16 20:10:50 -0500

    Delete Calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { Calculator } from './components';
+import { Screen } from './components';
 
 function App() {
+  const showOnScreen: string = '1 + 1';
+
   return (
     <div className='container'>
       <header className='row'>
         <h1 className='col-12 text-center mt-4'>Calculator App</h1>
       </header>
 
-      <Calculator />
+      <div className='row justify-content-center mt-5'>
+        <div className='col-11 col-sm-10 col-md-8 col-lg-6 bg-dark rounded-3'>
+          <div className='container p-3'>
+            <Screen showOnScreen={showOnScreen} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
