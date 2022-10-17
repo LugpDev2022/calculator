1d90ecb294455dcef504ae2517f5a3df34142be2
Lugp <lugpdev2022@gmail.com>
2022-10-16 19:50:51 -0500

    Create Calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Calculator } from './components/Calculator';
+
 function App() {
   return (
     <div className='container'>
       <div className='row'>
         <h1 className='col-12 text-center mt-4'>Calculator App</h1>
       </div>
+
+      <Calculator />
     </div>
   );
 }
---
+++ src/components/Calculator.tsx
@@ -0,0 +1,9 @@
+export const Calculator = () => {
+  return (
+    <div className='row justify-content-center mt-5'>
+      <div className='col-11 col-sm-10 col-md-8 col-lg-6 bg-dark rounded-3'>
+        Calculator
+      </div>
+    </div>
+  );
+};
