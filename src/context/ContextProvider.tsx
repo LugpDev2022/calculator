ba82c06fa4c63c68a34454d6daa7f93d2fc3914d
Lugp <lugpdev2022@gmail.com>
2022-10-19 17:43:50 -0500

    Delete the screen numbers when pressing a button after pressing the result button

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -7,31 +7,38 @@ type Props = {
 
 export const ContextProvider: React.FC<Props> = ({ children }) => {
   const [showOnScreen, setShowOnScreen] = useState('0');
+  const [isLastAResult, setIsLastAResult] = useState(false);
 
   const onNumberButtonClick = ({ target }: any) => {
-    if (showOnScreen === '0') {
+    if (showOnScreen === '0' || isLastAResult) {
       setShowOnScreen(target.innerHTML);
+      setIsLastAResult(false);
       return;
     }
 
     if (showOnScreen.length < 20) {
       setShowOnScreen(value => value + target.innerHTML);
+      setIsLastAResult(false);
+      return;
     }
   };
 
   const onOperationButtonClick = ({ target }: any) => {
     if (showOnScreen.length < 19) {
       setShowOnScreen(value => value + target.innerHTML);
+      setIsLastAResult(false);
     }
   };
 
   const onResultClick = () => {
     setShowOnScreen(eval(showOnScreen).toString());
+    setIsLastAResult(true);
   };
 
   const onDeleteButton = () => {
-    if (showOnScreen.length === 1) {
+    if (showOnScreen.length === 1 || isLastAResult) {
       setShowOnScreen('0');
+      setIsLastAResult(false);
       return;
     }
 
