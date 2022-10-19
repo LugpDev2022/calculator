c80680796c9e1145797563639f39e99d368c4bf7
Lugp <lugpdev2022@gmail.com>
2022-10-19 10:20:45 -0500

    Add a delete button but without functions

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -2,11 +2,19 @@ type Props = {
   showOnScreen: string;
 };
 
+//TODO: Set max number of characters to 50
+
 export const Screen: React.FC<Props> = ({ showOnScreen }) => {
   return (
     <div className='row bg-white rounded-5'>
-      <div className='col-12 py-2'>
-        <span className='fs-5'>{showOnScreen}</span>
+      <div className='col-12 fs-5 pe-0 d-flex justify-content-between align-items-center'>
+        {showOnScreen}
+        <button
+          className='text-danger rounded-start btn rounded-5'
+          onClick={() => console.log('Delete button pressed')}
+        >
+          DEL
+        </button>
       </div>
     </div>
   );
