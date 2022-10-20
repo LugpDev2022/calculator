ef09195bb574c9ab5b59ff59dd3d03f0d323d2cb
Lugp <lugpdev2022@gmail.com>
2022-10-19 21:31:37 -0500

    Align the screen borders with the button borders

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -5,7 +5,7 @@ export const Screen = () => {
   const { showOnScreen, onDeleteButton } = useContext(AppContext);
 
   return (
-    <div className='row bg-white rounded-4'>
+    <div className='row bg-white rounded-4 m-0'>
       <div className='col-12 pe-0 d-flex justify-content-between align-items-center'>
         <span className='py-2 fs-4'>{showOnScreen}</span>
         <button
