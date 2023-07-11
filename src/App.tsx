2b33475534d4be480a6294f8ae412e948bec76c0
LugpDev2022 <lugpdev2022@gmail.com>
2023-07-11 17:57:38 -0600

    Make calculator body

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,31 @@ import { ContextValue, ThemeContext } from './context/ThemeContext';
 function App() {
   const { theme } = useContext(ThemeContext) as ContextValue;
 
-  return <h1 className='text-red-500'>Calculator</h1>;
+  return (
+    <div
+      className={`
+        ${theme === 'dark' ? 'bg-[#0A0B0D]' : 'bg-white'}
+        h-full
+        w-full
+        flex
+        justify-center
+        items-center
+      `}
+    >
+      <main
+        className={`
+          ${theme === 'dark' ? 'bg-slate-800' : 'bg-yellow-200'}
+          ${theme === 'dark' ? 'text-white' : 'text-amber-400'}
+          w-4/5
+          max-w-lg
+          p-5
+          rounded-2xl
+        `}
+      >
+        Calculator
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          800: 'rgb(33, 49, 64)',
+        },
+        amber: {
+          400: 'rgb(156, 143, 19)',
+        },
+        yellow: {
+          200: 'rgb(219, 249, 145)',
+        },
+      },
+    },
   },
   plugins: [],
 };
