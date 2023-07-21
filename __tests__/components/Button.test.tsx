2c185202e4e1c74a2479c05b7f699a41223200d0
LugpDev2022 <lugpdev2022@gmail.com>
2023-07-21 11:04:22 -0600

    Update <Button /> tests

--- __tests__/components/Button.test.tsx
+++ __tests__/components/Button.test.tsx
@@ -2,94 +2,114 @@ import React from 'react';
 import { vi } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import Button from '../../src/components/Button';
+import {
+  CalcStateContext,
+  CalcStateContextType,
+} from '../../src/context/CalcStateContext';
 
 describe('Tests on <Button />', () => {
   beforeEach(() => vi.clearAllMocks());
 
+  const value: CalcStateContextType = {
+    dispatch: vi.fn(),
+    error: '',
+    operation: '',
+    result: 0,
+  };
+
   test('should match snapshot', () => {
     const { container } = render(
-      <Button dispatch={() => {}} keySign='DEL' operation='2+2' />
+      <CalcStateContext.Provider value={value}>
+        <Button keySign='DEL' />
+      </CalcStateContext.Provider>
     );
 
     expect(container).toMatchSnapshot();
   });
 
   test('should call dispatch with delete', () => {
-    const dispatchMock = vi.fn();
-
-    render(<Button dispatch={dispatchMock} keySign='DEL' operation='2+2' />);
+    render(
+      <CalcStateContext.Provider value={value}>
+        <Button keySign='DEL' />
+      </CalcStateContext.Provider>
+    );
 
     const button = screen.getByRole('button');
     fireEvent.click(button);
 
-    expect(dispatchMock).toBeCalledWith({ type: 'DELETE' });
+    expect(value.dispatch).toBeCalledWith({ type: 'DELETE' });
   });
 
   test('should call dispatch with reset', () => {
-    const dispatchMock = vi.fn();
-
-    render(<Button dispatch={dispatchMock} keySign='AC' operation='2+2' />);
+    render(
+      <CalcStateContext.Provider value={value}>
+        <Button keySign='AC' />
+      </CalcStateContext.Provider>
+    );
 
     const button = screen.getByRole('button');
     fireEvent.click(button);
 
-    expect(dispatchMock).toBeCalledWith({ type: 'RESET' });
+    expect(value.dispatch).toBeCalledWith({ type: 'RESET' });
   });
 
   test('should call dispatch with set error', () => {
-    const dispatchMock = vi.fn();
-
-    render(<Button dispatch={dispatchMock} keySign='=' operation='10+7+5(' />);
+    render(
+      <CalcStateContext.Provider value={{ ...value, operation: '10+7+5)' }}>
+        <Button keySign='=' />
+      </CalcStateContext.Provider>
+    );
 
     const button = screen.getByRole('button');
     fireEvent.click(button);
 
-    expect(dispatchMock).toBeCalledWith({
+    expect(value.dispatch).toBeCalledWith({
       type: 'SET_ERROR',
       payload: 'Invalid operation',
     });
   });
 
   test('should call dispatch with set result', () => {
-    const dispatchMock = vi.fn();
-
-    render(<Button dispatch={dispatchMock} keySign='=' operation='10+7+5' />);
-
-    const button = screen.getByRole('button');
-    fireEvent.click(button);
-
-    expect(dispatchMock).toBeCalledWith({ type: 'SET_RESULT', payload: 22 });
-  });
-
-  test('should not call dispatch', () => {
-    const dispatchMock = vi.fn();
-
     render(
-      <Button
-        dispatch={dispatchMock}
-        keySign='2'
-        operation='10+7+52+523452+2342432'
-      />
+      <CalcStateContext.Provider value={{ ...value, operation: '10+7+5' }}>
+        <Button keySign='=' />
+      </CalcStateContext.Provider>
     );
 
     const button = screen.getByRole('button');
     fireEvent.click(button);
 
-    expect(dispatchMock).not.toHaveBeenCalled();
+    expect(value.dispatch).toBeCalledWith({ type: 'SET_RESULT', payload: 22 });
+  });
+
+  test('should not call dispatch', () => {
+    render(
+      <CalcStateContext.Provider
+        value={{ ...value, operation: '10+7+52+523452+2342432' }}
+      >
+        <Button keySign='2' />
+      </CalcStateContext.Provider>
+    );
+
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    expect(value.dispatch).not.toHaveBeenCalled();
   });
 
   test('should call dispatch set operation', () => {
     const keySign = '2';
-    const dispatchMock = vi.fn();
 
     render(
-      <Button dispatch={dispatchMock} keySign={keySign} operation='10+7+5' />
+      <CalcStateContext.Provider value={{ ...value, operation: '10+7+5' }}>
+        <Button keySign={keySign} />
+      </CalcStateContext.Provider>
     );
 
     const button = screen.getByRole('button');
     fireEvent.click(button);
 
-    expect(dispatchMock).toBeCalledWith({
+    expect(value.dispatch).toBeCalledWith({
       type: 'SET_OPERATION',
       payload: keySign,
     });
